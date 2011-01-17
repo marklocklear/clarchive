d51a9dabef2b4af8344b0e17ea4162543d1bf58c
J. Mark Locklear <marklocklear@gmail.com>
2011-01-17 16:26:28 -0500

    test

---
+++ clist.rb
@@ -0,0 +1,28 @@
+require 'csv'
+require 'htmlunit.rb'
+ writer = CSV.open('writeClist.csv', 'w')
+ webClient = WebClient.new(BrowserVersion::FIREFOX_3)
+ main_page = webClient.getPage("http://raleigh.craigslist.org/")
+ main_div = main_page.getElementById("sss")
+ h4 = main_page.getByXPath(main_div.getCanonicalXPath() + "/h4/a")
+ for_sale_page = h4[0].click
+ i=1
+ while i < 5
+    post = for_sale_page.getByXPath("html/body/blockquote[2]/p[#{i}]/a")
+    click_post = post[0].click
+    title = click_post.getByXPath("html/body/h2")
+    post_body = click_post.getElementById("userbody")
+    html_body = click_post.getByXPath("html/body")
+		#str.partition(sep) → [head, sep, tail]
+		title = title[0].asText
+    date = html_body[0].asText.partition("Date:")[2][0..19] #grabs all txt after string 'Date:' then grabs 0..19 which is date and time
+    post_id = html_body[0].asText.partition("PostingID")[2][0..12] #see above
+#TODO Use a regex to search a post for any and all phone numbers. Possibly drop these into a separate table with PostID as prim key
+#TODO need to add code when parsing location string to add text until we see a carrage return
+    location = html_body[0].asText.partition("Location:")[2][0..5] #see above
+    puts "Date is =>" + date + " Post id is =>" + post_id + " Locaiton is=>" + location + " Tile is =>" + title
+   	#puts html_body[0].asText
+    #puts post_body.asText #gets body
+    i+=1
+  end
+  writer.close
---
+++ htmlunit.rb
@@ -0,0 +1,33 @@
+require 'java'
+
+#HTMLUnit 2.8 jar files
+require 'lib/apache-mime4j-0.6.jar'
+require 'lib/commons-codec-1.4.jar'
+require 'lib/commons-collections-3.2.1.jar'
+require 'lib/commons-io-1.4.jar'
+require 'lib/commons-lang-2.4.jar'
+require 'lib/commons-logging-1.1.1.jar'
+require 'lib/cssparser-0.9.5.jar'
+require 'lib/htmlunit-2.8.jar'
+require 'lib/htmlunit-core-js-2.8.jar'
+require 'lib/httpclient-4.0.1.jar'
+require 'lib/httpcore-4.0.1.jar'
+require 'lib/httpmime-4.0.1.jar'
+require 'lib/nekohtml-1.9.14.jar'
+require 'lib/sac-1.3.jar'
+require 'lib/serializer-2.7.1.jar'
+require 'lib/xalan-2.7.1.jar'
+require 'lib/xercesImpl-2.9.1.jar'
+require 'lib/xml-apis-1.3.04.jar'
+
+#Web Client Class
+include_class 'com.gargoylesoftware.htmlunit.WebClient'
+include_class 'com.gargoylesoftware.htmlunit.BrowserVersion'
+include_class 'com.gargoylesoftware.htmlunit.SilentCssErrorHandler'
+
+#turn off warnings
+[ 'com.gargoylesoftware.htmlunit',
+  'com.gargoylesoftware.htmlunit.html',
+  'com.gargoylesoftware.htmlunit.javascript',
+  'org.apache.commons.httpclient'
+].each { |package| java.util.logging.Logger.getLogger(package).level = java.util.logging.Level::OFF }
