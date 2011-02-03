547c0177b50f2dec92a1a4835756c7766637ff84
J. Mark Locklear <marklocklear@gmail.com>
2011-02-02 22:01:23 -0500

    refined url regex

--- get_main.rb
+++ get_main.rb
@@ -8,8 +8,9 @@ require 'htmlunit.rb'
  	main_page = webClient.getPage("http://www.craigslist.org/about/sites")
 	#urls = main_page.asXml.to_s.scan(/[a-zA-Z0-9\-\.]+\.(com|org|net|mil|edu|COM|ORG|NET|MIL|EDU)/)
 	#urls = main_page.asXml.to_s.scan(%r`((http|https?:\/\/)+((?:[-a-z0-9]+\.)+[a-z]{2,}))`)
-	urls = main_page.asXml.to_s.scan(%r`http.*craigslist.*`)
+	urls = main_page.asXml.to_s.scan(%r`(http.*craigslist.*\.(com|uk|de|it|ca|org|in|jp|kr|ph|sg|tw|th|au|mx|za))`)
+	#urls = main_page.asXml.to_s.scan(%r`(http.*craigslist.*\.(au|uk))`)
 	for i in 0..urls.length-1 do
-	puts i.to_s + " " + urls[i]
+	puts i.to_s + " " + urls[i].to_s
 	end
  	#puts urls.inspect
