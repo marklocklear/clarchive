2219022e72a72e3e7ceb1f8c5c63b52c456e9b97
J. Mark Locklear <jlocklear@jlocklear.(none)>
2011-02-08 18:04:59 -0500

    dbi test

---
+++ simple_dbi.rb
@@ -0,0 +1,33 @@
+
+require 'rubygems'
+require "dbi"
+require 'java'
+require 'htmlunit.rb'
+
+begin
+
+# connect to the MySQL server
+#
+dbh = DBI.connect("DBI:Mysql:clist:localhost", "root", "homer9721")
+#
+# # get server version string and display it
+#
+ row = dbh.select_one("SELECT VERSION()")
+#
+ puts "Server version: " + row[0]
+#
+ rescue DBI::DatabaseError => e
+#
+ puts "An error occurred"
+#
+ puts "Error code: #{e.err}"
+#
+ puts "Error message: #{e.errstr}"
+#
+ ensure
+#
+# # disconnect from server
+#
+ dbh.disconnect if dbh
+#
+ end
