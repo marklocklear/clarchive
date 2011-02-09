d7a0355768bbb53a63523dbc92f321466634e4be
J. Mark Locklear <jlocklear@jlocklear.(none)>
2011-02-08 22:14:04 -0500

    added conditionals to handle blank fields

--- clist.rb
+++ clist.rb
@@ -10,27 +10,41 @@ require 'htmlunit.rb'
  	h4 = main_page.getByXPath(main_div.getCanonicalXPath() + "/h4/a")
  	for_sale_page = h4[0].click
  	i=1
- 	while i < 25
+ 	while i < 99
     post = for_sale_page.getByXPath("html/body/blockquote[2]/p[#{i}]/a")
-    click_post = post[0].click
-    title = click_post.getByXPath("html/body/h2")
-    post_body = click_post.getElementById("userbody")
-    html_body = click_post.getByXPath("html/body")
+		if post[0]
+    	click_post = post[0].click
+    	title = click_post.getByXPath("html/body/h2")
+    	post_body = click_post.getElementById("userbody")
+    	html_body = click_post.getByXPath("html/body")
+		end
+
 		#str.partition(sep) → [head, sep, tail]
 		title = title[0].asText
-		phone = post_body.asText.scan(/\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})/)
-		url = post_body.asText.scan(/((http|https?:\/\/)+((?:[-a-z0-9]+\.)+[a-z]{2,}))/)
-    date = html_body[0].asText.partition("Date:")[2][0..19] #grabs all txt after string 'Date:' then grabs 0..19 which is date and time
+		if post_body
+			phone = post_body.asText.scan(%r'\(?([0-9]{3})\)?[-.\/ ]?([0-9]{3})[-.\/ ]?([0-9]{4})')
+			url = post_body.asText.scan(/((www|http|https?:\/\/)+((?:[-a-z0-9]+\.)+[a-z]{2,}))/)
+		end
+		if html_body[0]
+    	date = html_body[0].asText.partition("Date:")[2][0..19] #grabs all txt after string 'Date:' then grabs 0..19 which is date and time
+		end
+
     post_id = html_body[0].asText.partition("PostingID")[2][0..12] #see above
 #TODO Use a regex to search a post for any and all phone numbers. Possibly drop these into a separate table with PostID as prim key
 #TODO need to add code when parsing location string to add text until we see a carrage return. Possible use regex to search for
 #			text between word 'location' and carrage return => \n
     #location = html_body[0].asText.partition("Location:")[2][0..5] #see above
-    location = click_post.getByXPath("html/body/div[4]/ul/li[1]")
-		location = location[0].asText.gsub('Location: ', ' ')
+    location_first_attempt = click_post.getByXPath("html/body/div[4]/ul/li[1]")
+    location_second_attempt = click_post.getByXPath("html/body/ul[1]/li[1]")
+		if location_first_attempt[0]
+			location = location_first_attempt[0].asText.gsub('Location: ', ' ')
+		else
+			#location = location_second_attempt[0].asText.gsub('Location: ', ' ')
+			location = "Location not found"
+		end
     puts "Date is =>" + date
 		puts "Post id is =>" + post_id
-		puts "Locaiton is=>" + location
+		puts "Location is=>" + location
 		puts "Tile is =>" + title
 		puts "Phone is =>" + phone.inspect
 		puts "URL is " + url.inspect
--- regexample.rb
+++ regexample.rb
@@ -1,6 +1,6 @@
-string = "thi335.670.3344s is a (332)223-5283 test 474-347-1122 and more here!!! And now 334.332.3357 or it could be 334 443 3322"
-string2 = "3435884433 or 332 233-2233 now lets add http://www.wintas.com"
-reg = string.scan(%r`[0-9]{3}[-. ][0-9]{3}[-. ][0-9]{4}`)
+string = "thi335.670.3344s is a (332)223-5283 test 474-347-1122 and more here!!! And now 334.332.3357 or it could be 334 443 3322, but also 334/334-3322"
+string2 = "3435884433 or 332 233-2233 now lets add http://www.wintas.com or there could be ***www.sassy.com***"
+reg = string.scan(%r`[0-9]{3}[-.\/ ][0-9]{3}[-.\/ ][0-9]{4}`)
 reg2 = string.scan(/\([0-9]{3}\)[0-9]{3}[-.][0-9]{4}/)
 reg4 = string.scan(/\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})/)
 url = string2.scan(/((http|https?:\/\/)+((?:[-a-z0-9]+\.)+[a-z]{2,}))/)
